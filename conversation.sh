f46868132139db45a083271911d1d879fa03859e
DurgaPrasad <dprasadraju.mantena3579@gmail.com>
2026-01-29 20:14:01 +0530

    first program

---
+++ conversation.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+person1="suresh"
+person2="ramesh"
+echo "$person1: Hello, how are you?"
+echo "$person2: I'm good, thank you! How about you?"
+echo "$person1: I'm doing well too. What have you been up to lately?"
+echo "$person2: Just working on some projects. How about you?"
