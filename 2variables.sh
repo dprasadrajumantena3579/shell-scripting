00f5cd07fbdb0340ae2678504b3099e5bfe1fcfb
DurgaPrasad <dprasadraju.mantena3579@gmail.com>
2026-01-30 10:25:11 +0530

    third program

---
+++ 2variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "please enter your name:"
+read username
+echo "user name is: $username"
+echo "please enter your password:"
+read -s password
+echo "password is: $password"
+echo "Welcome $username, your password is set."
