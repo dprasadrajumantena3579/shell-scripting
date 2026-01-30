f89bdb5f9b95de0d81132f6b809cc6d47d20a864
DurgaPrasad <dprasadraju.mantena3579@gmail.com>
2026-01-30 09:50:47 +0530

    second program

--- 1variables.sh
+++ 1variables.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 person1="$1"
 person2="$2"
-echo "$person1: Hello $1, how are you?"
-echo "$person2: I'm good $2, thank you! How about you?"
-echo "$person1: I'm doing well too. What have you been up to lately?$1"
-echo "$person2: Just working on some projects. How about you?$2"
+echo "$person1: Hello $2, how are you?"
+echo "$person2: I'm good $1, thank you! How about you?"
+echo "$person1: I'm doing well too. What have you been up to lately?$2"
+echo "$person2: Just working on some projects. How about you?$1"
