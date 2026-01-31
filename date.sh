0d2a1a82ea95847cc2a2460123ae0186bc272231
DurgaPrasad <dprasadraju.mantena3579@gmail.com>
2026-01-31 11:33:22 +0530

    dtae execute

--- date.sh
+++ date.sh
@@ -6,5 +6,5 @@ echo "Script started at: $start_time"
 sleep 10
 end_time=$(date +%s)
 echo "Script ended at: $end_time"
-total_time=$((end_time - start_time))
+total_time=$((end_time -start_time))
 echo "Total execution time: $total_time seconds"
