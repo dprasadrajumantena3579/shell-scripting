a1665ebd5394a71b53a771cda4209a80e3afa7ac
DurgaPrasad <dprasadraju.mantena3579@gmail.com>
2026-01-31 08:58:54 +0530

    dtae execute

--- date.sh
+++ date.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 timestamp=$(date)
 echo "Current date: $timestamp"
+start_time=$(date +%s)
+echo "Script started at: $start_time"
+sleep 10
+end_time=$(date +%s)
+echo "Script ended at: $end_time"
+total_time=$((end_time - start_time))
+echo "Total execution time: $total_time seconds"
