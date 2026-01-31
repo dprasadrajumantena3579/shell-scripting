a074fb0325a6c03f734578c2e4efab02ae61b72b
DurgaPrasad <dprasadraju.mantena3579@gmail.com>
2026-01-31 09:56:48 +0530

    datatypes $ arrays

---
+++ datatypes.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+number=10
+number2=20
+sum=$((number + number2))
+echo "The sum of $number and $number2 is: $sum"
+
+#arrays
+array=(apple banana cherry)
+echo "First element: ${array[0]}"
