08298fe7480b4ca9dc5c212245bf2fa1639c897c
dprasadrajumantena3579 <dprasadraju.mantena3579@gmail.com>
2026-02-11 21:59:04 +0530

    nginx using conditions

---
+++ 3conditions.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+USER_ID=$(id -u)
+if [ $USER_ID -eq 0 ]; then
+    echo "You are running this script as root."
+else
+    echo "You are running this script as a regular user."
+    exit 1
+fi
+echo "installing nginx"
+dnf install nginx -y
+if [ $? -eq 0 ]; then
+    echo "nginx installed successfully."
+else
+    echo "Failed to install nginx."
+    exit 1
+fi
